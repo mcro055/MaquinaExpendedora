471fc253a9ab3c940f4ba36cf50f1685bd806bf8
TinoAsecas11 <tinoasecas11@gmail.com>
2024-10-10 11:09:27 +0200

    Segundo Comiit

--- src/main/java/com/example/maquina_expendedora/HelloApplication.java
+++ src/main/java/com/example/maquina_expendedora/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.example.maquina_expendedora;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -28,25 +29,57 @@ public class HelloApplication extends Application {
     @javafx.fxml.FXML
     private ImageView Sprite2Img;
     @javafx.fxml.FXML
+    private Button Butt1e;
+    @javafx.fxml.FXML
+    private Label Tag7up;
+    @javafx.fxml.FXML
     private Button Butt2;
     @javafx.fxml.FXML
     private Button Butt1;
     @javafx.fxml.FXML
-    private Label Pantalla;
+    private Button Butt50;
+    @javafx.fxml.FXML
+    private Label TagFanta;
+    @javafx.fxml.FXML
+    private Button Butt10;
+    @javafx.fxml.FXML
+    private Label TagCocacola;
+    @javafx.fxml.FXML
+    private Label TagKas;
+    @javafx.fxml.FXML
+    private Button ButtY;
+    @javafx.fxml.FXML
+    private Button Butt2e;
+    @javafx.fxml.FXML
+    private Button ButtN;
     @javafx.fxml.FXML
     private Button ButtC;
     @javafx.fxml.FXML
+    private Label PantallaImporte;
+    @javafx.fxml.FXML
+    private Label TagPepsi;
+    @javafx.fxml.FXML
+    private Label PantallaID;
+    @javafx.fxml.FXML
+    private Label TagSprite;
+    @javafx.fxml.FXML
     private Button ButtB;
+    @javafx.fxml.FXML
+    private Button Butt20;
     @javafx.fxml.FXML
     private Button ButtA;
     @javafx.fxml.FXML
     private Button Butt3;
+    @javafx.fxml.FXML
+    private Button Butt05;
 
+
+    @Deprecated
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 775, 891);
-        stage.setTitle("Hello!");
+        stage.setTitle("Maquina Expendedora");
         stage.setScene(scene);
         stage.resizableProperty().setValue(Boolean.FALSE);
         stage.show();
--- src/main/java/com/example/maquina_expendedora/HelloController.java
+++ src/main/java/com/example/maquina_expendedora/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.maquina_expendedora;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 public class HelloController {
-
     @FXML
     private ImageView FantaImg;
     @FXML
@@ -22,4 +24,152 @@ public class HelloController {
     private GridPane VitrinaId;
     @FXML
     private ImageView Sprite2Img;
+    @FXML
+    private Label PantallaImporte;
+    @FXML
+    private Label PantallaID;
+    @FXML
+    private Button Butt1e;
+    @FXML
+    private Label Tag7up;
+    @FXML
+    private Button Butt2;
+    @FXML
+    private Button Butt1;
+    @FXML
+    private Button Butt50;
+    @FXML
+    private Label TagFanta;
+    @FXML
+    private Button Butt10;
+    @FXML
+    private Label TagCocacola;
+    @FXML
+    private Label TagKas;
+    @FXML
+    private Button ButtY;
+    @FXML
+    private Button Butt2e;
+    @FXML
+    private Button ButtN;
+    @FXML
+    private Button ButtC;
+    @FXML
+    private Label TagPepsi;
+    @FXML
+    private Label TagSprite;
+    @FXML
+    private Button ButtB;
+    @FXML
+    private Button Butt20;
+    @FXML
+    private Button ButtA;
+    @FXML
+    private Button Butt3;
+    @FXML
+    private Button Butt05;
+
+    private Alert Alerterr = new Alert(Alert.AlertType.ERROR);
+    private Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    double Importe;
+    String [][]Aux = {{"A1","1.35"},{"B1","1.20"},{"A2","1.20"},{"B2","1.10"},{"A3","1.20"},{"B3","1.20"}};
+    public void initialize() {
+    }
+
+    @FXML
+    public void PrsdButt05() {
+        Importe = Importe+0.05;
+        PantallaImporte.setText(String.format("%.2f",Importe));
+    }
+
+    @FXML
+    public void PrsdButt10() {
+        Importe = Importe+0.10;
+        PantallaImporte.setText(String.format("%.2f",Importe));
+    }
+
+    @FXML
+    public void PrsdButt20() {
+        Importe = Importe+0.20;
+        PantallaImporte.setText(String.format("%.2f",Importe));
+    }
+
+    @FXML
+    public void PrsdButt50() {
+        Importe = Importe+0.50;
+        PantallaImporte.setText(String.format("%.2f",Importe));
+    }
+
+    @FXML
+    public void PrsdButt1e() {
+        Importe = Importe+1;
+        PantallaImporte.setText(String.format("%.2f",Importe));
+    }
+
+    @FXML
+    public void PrsdButt2e() {
+        Importe = Importe+2;
+        PantallaImporte.setText(String.format("%.2f",Importe));
+    }
+
+    @FXML
+    public void PrsdButtA() {
+        PantallaID.setText("A");
+    }
+
+    @FXML
+    public void PrsdButtB() {
+        PantallaID.setText("B");
+    }
+
+    @FXML
+    public void PrsdButtC() {
+        PantallaID.setText("C");
+    }
+
+    @FXML
+    public void PrsdButt1() {
+        PantallaID.setText(PantallaID.getText()+1);
+    }
+
+    @FXML
+    public void PrsdButt2() {
+        PantallaID.setText(PantallaID.getText()+2);
+    }
+
+    @FXML
+    public void PrsdButt3() {
+        PantallaID.setText(PantallaID.getText()+3);
+    }
+
+    @FXML
+    public void PrsdButtY() {
+        for (int i=0;i<Aux.length;i++){
+            if(PantallaID.getText().equals(Aux[i][0])){
+                if(Importe>=Double.parseDouble(Aux[i][1])){
+                    Importe = Importe-Double.parseDouble(Aux[i][1]);
+                    PantallaImporte.setText(String.format("%.2f",Importe));
+                    alert.setTitle("Felicidades");
+                    alert.setHeaderText("Comprado");
+
+                    alert.showAndWait();
+                }else{
+                    Alerterr.setTitle("Error");
+                    Alerterr.setHeaderText("Saldo insuficiente");
+
+                    // Mostrar la alerta
+                    Alerterr.showAndWait();
+                }
+            }
+        }
+    }
+
+    @FXML
+    public void PrsdButtN() {
+        PantallaID.setText("");
+        PantallaImporte.setText("Cancelada");
+        Importe = 0;
+
+    }
+
 }
